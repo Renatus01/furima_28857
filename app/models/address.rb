07b619f411b3dfc09ccacd4c59625a8f24ae0635
Renatus01 <nostalgia1910@yahoo.co.jp>
2020-09-11 19:37:19 +0900

    商品削除機能の修正

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,6 @@
 class Address < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
-  belongs_to             :order
+  belongs_to             :order,      dependent: :destroy
   belongs_to_active_hash :prefecture
 end
